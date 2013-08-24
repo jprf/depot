f4da980bf03aed2bbf94183c202f67ca7e90de2f
Joao Feliciano <jprfelix@gmail.com>
2013-08-24 10:52:15 +0100

    products and store front

---
+++ app/models/product.rb
@@ -0,0 +1,12 @@
+class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true, length: {
+    minimum: minimum = 10,
+    message: "must be at least #{minimum} characters."
+  }
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+end
